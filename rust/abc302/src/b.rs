615e50aa00f35e5ea0fb59505f6c5331c3f99d67
makinzm <yuruseba@yahoo.co.jp>
2023-05-28 10:54:31 +0900

    2nd submit in 302b using rust

--- rust/abc302/src/b.rs
+++ rust/abc302/src/b.rs
@@ -4,14 +4,6 @@ use proconio::{
 	marker::Chars
 };
 
-fn min<T: PartialOrd>(a: T, b: T) -> T {
-    if a < b {
-        a
-    } else {
-        b
-    }
-}
-
 #[fastout]
 fn main() {
 	input! {
@@ -20,19 +12,26 @@ fn main() {
         m: [Chars; h],
     };
 	// only search starting from `s`
-	let mut flag = false;
-	let minus_one = -1;
 	for x in 0..h{
 		for y in 0..w{
 			if m[x as usize][y as usize] == 's'{
 				for dx in -1..=1{
 					for dy in -1..=1{
-						let xplus = (x as i32 + dx) as usize;
-						let yplus = (y as i32 + dy) as usize;
+						if dx < 0{
+							if x < (4*(dx as i32).abs()){
+								continue;
+							}
+						}
+						if dy < 0{
+							if y < (4*(dy as i32).abs()){
+								continue;
+							}
+						}
 						let xplus4 = (x as i32 + 4*dx) as usize;
 						let yplus4 = (y as i32 + 4*dy) as usize;
-						if xplus >=0 && xplus4 < w as usize && yplus >= 0 && yplus4 < h as usize {
-							if m[xplus][yplus] == 'n' 
+						if xplus4 < w as usize && yplus4 < h as usize {
+							// println!("{},{} -> {},{}",x,y,xplus4,yplus4);
+							if m[(x as i32+1*dx) as usize][(y as i32+1*dy) as usize] == 'n' 
 							&& m[(x as i32+2*dx) as usize][(y as i32+2*dy) as usize] == 'u' 
 							&& m[(x as i32+3*dx) as usize][(y as i32+3*dy) as usize] == 'k' 
 							&& m[(x as i32+4*dx) as usize][(y as i32+4*dy) as usize] == 'e'
