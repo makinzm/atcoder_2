c34b3a293d2b46192378ffe9688f624f54343e66
makinzm <yuruseba@yahoo.co.jp>
2023-09-10 12:48:57 +0900

    add C of ABC319 using struct, impl, is_none (considerring as_mut)

--- rust/template/src/run.sh
+++ rust/template/src/run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# コンテスト中にRunするための関数 (utilコマンドはつけない.)
+
 filename=$(basename "$1")
 # extension="${filename##*.}"
 basename="${filename%.*}"
--- rust/template/src/touch.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for (( i = 97; i <= 103; i++ )); do
-    current_char=$(printf \\$(printf '%03o' $i))
-    filename=""$current_char".rs"
-    touch $filename
-    echo "// Here is "$current_char"'s problem\n\nfn main() {\n\tprintln!(\"Here is "$current_char"!\");\n}" > $filename
-done
