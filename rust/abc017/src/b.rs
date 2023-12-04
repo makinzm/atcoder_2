a9b3ca5cf45b18427a5fef7fc17c7b048743c14a
makinzm <yuruseba@yahoo.co.jp>
2023-12-05 08:00:31 +0900

    done https://atcoder.jp/contests/arc017/submissions/48201082 -> https://atcoder.jp/contests/arc017/submissions/48201128

--- rust/abc017/src/b.rs
+++ rust/abc017/src/b.rs
@@ -12,11 +12,22 @@ fn main() {
 
 	let mut a_count = 0;
 
-	for i in 0..(n-k+1) {
-		let partial_a = &a[i..(i+k)];
-		// check whether partial_a is an increasing array
-		if partial_a.windows(2).all(|w| w[0] < w[1]) {
-			a_count += 1;
+	// separate a into a lot of increasing sequences
+	let mut a_seq = vec![];
+	let mut a_seq_len = 0;
+	for i in 0..n-1 {
+		if a[i] < a[i+1] {
+			a_seq_len += 1;
+		} else {
+			a_seq.push(a_seq_len);
+			a_seq_len = 0;
+		}
+	}
+	a_seq.push(a_seq_len);
+	
+	for i in 0..a_seq.len() {
+		if a_seq[i] >= k-1 {
+			a_count += (a_seq[i] + 1) - k + 1;
 		}
 	}
 
