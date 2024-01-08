ea8a5696029d2c83537764f21921832eaa0b423d
makinzm <yuruseba@yahoo.co.jp>
2024-01-08 09:56:58 +0900

    progress: using checked and stack pow

--- rust/template/src/d.rs
+++ rust/template/src/d.rs
@@ -8,12 +8,18 @@ use std::collections::VecDeque;
 // O(q) 
 fn main() {
 	let module = 998244353;
+	let overflow_limit = 200000;
+	let mut pows_lst = vec![1];
+	for _ in 0..overflow_limit{
+		let last = *pows_lst.last().unwrap() as isize;
+		pows_lst.push(last.checked_mul(10).unwrap().checked_rem_euclid(module).unwrap());
+	}
 	input! {
 		q: isize,
 	}
 	let mut state_stack = VecDeque::new();
 	state_stack.push_back(1);
-	let mut ans = 1;
+	let mut ans: isize = 1;
 	// O(q)
 	for _ in 0..q{
 		input! { order: isize }
@@ -21,12 +27,13 @@ fn main() {
 			1 => {
 				input! { x: isize }
 				state_stack.push_back(x);
-				ans = (ans * 10 + x) % module;
+				ans = (ans.checked_mul(10).unwrap() + x).checked_rem_euclid(module).unwrap();
 			}
 			2 => {
-				let popped_state = state_stack.pop_front().unwrap();
-				ans -= popped_state * (10_isize.pow(state_stack.len() as u32) % module);
-				ans %= module;
+				let mut x = state_stack.pop_front().unwrap();
+				x = x.checked_mul(pows_lst[state_stack.len()]).unwrap();
+				ans = ans.checked_sub(x).unwrap_or_else(|| ans + module - x);
+				ans = ans.checked_rem_euclid(module).unwrap();
 			}
 			3 => {
 				println!("{}", ans);
