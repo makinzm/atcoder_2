4026f44844e9f9e4c8385700d580b021574d22f2
makinzm <yuruseba@yahoo.co.jp>
2024-02-18 16:09:53 +0900

    remove: vain println comment

--- rust/template/src/e.rs
+++ rust/template/src/e.rs
@@ -39,13 +39,9 @@ impl <M: Monoid> SegmentTree<M> {
 		let mut i = i as isize;
 		i += self.n as isize - 1;
 		self.data[i as usize] = x;
-		// println!("data {:?}", self.data);
 		while i > 0 {
 			i = (i - 1) / 2;
-			// println!("i,2i+1,2i+2 {:?} {:?} {:?}", i, i * 2 + 1, i * 2 + 2);
-			// println!("left {:?} right {:?}", self.data[i as usize * 2 + 1], self.data[i as usize * 2 + 2]);
 			self.data[i as usize] = M::op(&self.data[i as usize * 2 + 1], &self.data[i as usize * 2 + 2]);
-			// println!("\tdata {:?}", self.data);
 		}
 	}
 
@@ -63,11 +59,8 @@ impl <M: Monoid> SegmentTree<M> {
 		} else if l <= il && ir <= r {
 			self.data[k].clone()
 		} else {
-			// println!("START: op params l,r,k,il,ir {:?} {:?} {:?} {:?} {:?}", l, r, k, il, ir);
 			let x = self.get_sub(l, r, k * 2 + 1, il, (il + ir) / 2);
 			let y = self.get_sub(l, r, k * 2 + 2, (il + ir) / 2, ir);
-			// println!("x, y {:?} {:?}", x, y);
-			// println!("DONE: op params l,r,k,il,ir {:?} {:?} {:?} {:?} {:?}", l, r, k, il, ir);
 			M::op(&x, &y)
 		}
 	}
@@ -107,29 +100,20 @@ fn main() {
 	for i in 0..n-1 {
 		st.set(i, a[i]);
 	}
-	// println!("INIT st {:?}", st);
 	for (t, l, r) in queries {
 		let l = l - 1;
 		let r = r - 1;
 		if t == 1 {
 			if l != 0 && l != n-1 {
 				let l_value = st.get(l-1, l);
-				// println!("l_value {:?}", l_value);
-				// println!("st {:?}", st);
 				st.set(l-1, (l_value ^ 1) as usize);
-				// println!("st {:?}", st);
 			}
 			if r != 0 && r != n-1 {
 				let r_value = st.get(r, r+1);
-				// println!("r_value {:?}", r_value);
-				// println!("st {:?}", st);
 				st.set(r, (r_value ^ 1) as usize);
-				// println!("st {:?}", st);
 			}
 		} else {
 			let get_sum = st.get(l, r);
-			// println!("get_sum {:?}", get_sum);
-			// println!("r-l {:?}", r-l);
 			if get_sum == r - l {
 				println!("Yes");
 			} else {
