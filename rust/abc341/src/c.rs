3b51c2d9ec07e138021940f6206e3165f3ba82b9
makinzm <yuruseba@yahoo.co.jp>
2024-02-18 10:15:53 +0900

    update c.rs to meet acceptance criteria

--- rust/abc341/src/c.rs
+++ rust/abc341/src/c.rs
@@ -13,9 +13,15 @@ fn main() {
 		t: String,
 		s: [String; h],
 	}
+	let mut s_vec = Vec::new();
+	for i in 0..h {
+		s_vec.push(s[i].chars().collect::<Vec<char>>());
+	}
 	let t = t.chars().collect::<Vec<char>>();
 	let mut ans = 0;
 	let mut directory_map: HashMap<char, (isize, isize)> = HashMap::new();
+	let land_mark: char = '.';
+	let sea_mark: char = '#';
 	directory_map.insert('L', (-1, 0));
 	directory_map.insert('R', (1, 0));
 	directory_map.insert('U', (0, -1));
@@ -24,14 +30,14 @@ fn main() {
 		for j in 0..w {
 			let mut next_i: isize = i as isize;
 			let mut next_j: isize = j as isize;
-			if s[i].chars().nth(j).unwrap() == '.' {
+			if s_vec[i][j] == land_mark {
 				for n_step in 0..n {
 					next_i += directory_map[&t[n_step]].1;
 					next_j += directory_map[&t[n_step]].0;
 					if next_i >= h as isize || next_j >= w as isize || next_i < 0 || next_j < 0 {
 						break;
 					}
-					if s[next_i as usize].chars().nth(next_j as usize).unwrap() == '#' {
+					if s_vec[next_i as usize][next_j as usize] == sea_mark {
 						break;
 					}
 					if n_step == n-1 {
