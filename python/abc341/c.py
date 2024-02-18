9e0876938d87f0cac32dafcfcb97b3ac9e8fa128
makinzm <yuruseba@yahoo.co.jp>
2024-02-18 10:53:12 +0900

    git reset

--- python/abc341/c.py
+++ python/abc341/c.py
@@ -11,6 +11,10 @@ directory_map = {
     "D": (0, 1)
 }
 
+directories = []
+for i in range(n):
+    directories.append(directory_map[T[i]])
+
 ans = 0
 
 for i in range(h):
@@ -19,13 +23,13 @@ for i in range(h):
         next_i, next_j = i, j
         if s[i*w+j] == ".":
             for n_step in range(n):
-                next_i += directory_map[T[n_step]][1]
-                next_j += directory_map[T[n_step]][0]
+                next_i += directories[n_step][1]
+                next_j += directories[n_step][0]
                 if next_i < 0 or next_i >= h or next_j < 0 or next_j >= w:
                     break
                 if s[next_i*w+next_j] == "#":
                     break
-                if n_step == n-1:
-                    ans += 1
+            if n_step == n-1:
+                ans += 1
 
 print(ans)
