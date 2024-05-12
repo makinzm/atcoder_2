6edd1280251afb4a2b61e85a5ba0a70f1fe920f9
makinzm <nozomi.maki.da@gmail.com>
2024-05-12 11:33:14 +0900

    feat: 🎸 45/48 passed but RuntimeError
    [提出 #53398152 - AtCoder Beginner Contest
    353](https://atcoder.jp/contests/abc353/submissions/53398152)

--- python/abc353/e.py
+++ python/abc353/e.py
@@ -1,2 +1,49 @@
 n = int(input())
-a = list(map(int, input().split()))
+s = input().split(' ')
+
+class TrieNode:
+    def __init__(self, char: str):
+        # assert len(char) <= 1
+        self.char = char
+        self.weight = 1
+        self.children = dict()
+    
+    def __str__(self):
+        base = f"char: {self.char}, weight: {self.weight}"
+        children_description = f"\n Children are..."
+        for k, v in self.children.items():
+            children_description += "\n\t" + k + ":" + str(v)
+        return base + children_description
+    
+    def increment_weight(self):
+        self.weight += 1
+    
+    def add(self, strings: str):
+        incremented_node = self
+        for c in strings:
+            if c in incremented_node.children:
+                incremented_node.children[c].increment_weight()
+            else:
+                child = TrieNode(c)
+                incremented_node.children[c] = child
+            incremented_node = incremented_node.children[c]
+    
+    def calculate_answer(self):
+        ans = 0
+
+        def dfs(start_node):
+            nonlocal ans
+            for _, child in start_node.children.items():
+                if child.weight > 1:
+                    dfs(child)
+            ans += ((start_node.weight - 1) * start_node.weight ) // 2
+
+        dfs(self)
+        return ans
+
+trie_tree = TrieNode('')
+
+for strings in s:
+    trie_tree.add(strings)
+
+print(trie_tree.calculate_answer())
