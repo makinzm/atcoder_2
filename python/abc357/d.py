0bb214a9a996948978e56dc85b5780dc61d7f780
makinzm <nozomi.maki.da@gmail.com>
2024-06-09 11:04:26 +0900

    refactor: 💡 make function simpler

--- python/abc357/d.py
+++ python/abc357/d.py
@@ -1,17 +1,17 @@
-N = int(input())
+n = int(input())
 MOD = 998244353
 
-def f(n, i):
+def f(i):
     if i == 1:
         return n % MOD
     k = len(str(n))
     if i % 2 == 0:
-        left_side = f(n, i // 2)
+        left_side = f(i // 2)
         left_base = pow(10, k * (i // 2), MOD)
         return (left_side * left_base + left_side) % MOD
-    left_side = f(n, i - 1)
+    left_side = f(i - 1)
     left_base = pow(10, k, MOD)
     return (left_side * left_base + n) % MOD
 
-print(f(N, N))
+print(f(n))
 
