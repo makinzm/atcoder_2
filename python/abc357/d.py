f3491f6cc0e49a9772788cdd54cbd5e610781f28
makinzm <nozomi.maki.da@gmail.com>
2024-06-09 10:39:25 +0900

    test: 💍 use pow and remove some mod

--- python/abc357/d.py
+++ python/abc357/d.py
@@ -3,21 +3,18 @@ N = int(input())
 MOD = 998244353
 
 def f(n, i):
-    if i <= 0:
-        raise ValueError("i must be positive")
     if i == 1:
         return n % MOD
+    k = len(str(n))
+    if i % 2 == 0:
+        left_side = f(n, i // 2)
+        left_base = pow(10, k * (i // 2), MOD)
+        right_side = f(n, i // 2)
+        return (left_side * left_base + right_side) % MOD
     else:
-        k = len(str(n))
-        if i % 2 == 0:
-            left_side = f(n, i // 2) % MOD
-            left_base = (10 ** (k * (i // 2))) % MOD
-            right_side = f(n, i // 2) % MOD
-            return ((left_side * left_base) % MOD + right_side) % MOD
-        else:
-            left_side = f(n, i-1) % MOD
-            left_base = (10 ** k) % MOD
-            return ((left_side * left_base) % MOD + n) % MOD
+        left_side = f(n, i-1)
+        left_base = 10 ** k
+        return (left_side * left_base + n) % MOD
 
 print(f(N, N))
 
