7441ca6b06a024ce06918064164249262b30b5cf
makinzm <nozomi.maki.da@gmail.com>
2024-07-21 10:06:01 +0900

    refactor: how to create count

--- python/abc363/c.py
+++ python/abc363/c.py
@@ -1,28 +1,26 @@
 from more_itertools import distinct_permutations
-from numba import jit
 
 num, kaibun_length = map(int, input().split())
 base_string = input()
 
 count = 0
 
-@jit(nopython=True, cache=True)
 def is_palindrome(s):
     for i in range(len(s) // 2):
         if s[i] != s[-i - 1]:
             return False
     return True
 
-@jit(nopython=True)
-def is_k_palindrome(s, k):
+def is_k_palindrome(s, k, num):
     for i in range(num - k + 1):
         if is_palindrome(s[i:i + k]):
             return True
     return False
 
-count = sum(
-    not is_k_palindrome(perm, kaibun_length) for perm in distinct_permutations(base_string)
-)
+base_list = list(base_string)
+count = 0
+for perm in distinct_permutations(base_list):
+    count += not is_k_palindrome(perm, kaibun_length, num)
 
 print(count)
 
