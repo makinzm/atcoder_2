cc4b15d846fa6dbe71d75dee90e21cb8eda78d7c
makinzm <nozomi.maki.da@gmail.com>
2024-08-11 12:55:26 +0900

    bug: cannot solve this

--- python/abc366/e.py
+++ python/abc366/e.py
@@ -1,27 +1,55 @@
 n, d = map(int, input().split())
-xy = [tuple(map(int, input().split())) for _ in range(n)]
 
-xy_transformed = [(x + y, x - y) for x, y in xy]
+x = []
+y = []
+for _ in range(n):
+    xi, yi = map(int, input().split())
+    x.append(xi)
+    y.append(yi)
 
-max_sum = max(x + y for x, y in xy)
-min_sum = min(x + y for x, y in xy)
-max_diff = max(x - y for x, y in xy)
-min_diff = min(x - y for x, y in xy)
+max_x = 10 ** 6 + d
 
-count = 0
+def calc(xs):
+    # - max_x <= x <= max_x
+    # x_sum(x) = x_sum[x]
+    x_sum = [0] * (max_x * 2 + 1)
+    sorted_xs = sorted(xs)
+    # Sum(|-max_x-xi|) = Sum(max_x+xi)
+    x_sum[-max_x] = sum(xs) + max_x * n
 
-for x_plus_y in range(min_sum - d, max_sum + d + 1):
-    for x_minus_y in range(min_diff - d, max_diff + d + 1):
-        if x_plus_y % 2 != x_minus_y % 2:
-            continue
-        total_distance = 0
-        for xpy, xmy in xy_transformed:
-            total_distance += max(abs(x_plus_y - xpy), abs(x_minus_y - xmy))
-        if total_distance <= d:
-            count += 1
-            # print(f"x_plus_y={x_plus_y}, x_minus_y={x_minus_y}")
-            # print(f"total_distance={total_distance}")
-            # print(f"x,y=({(x_plus_y + x_minus_y) // 2}, {(x_plus_y - x_minus_y) // 2})")
-            # print()
+    # x_i < x を満たす x_i の個数
+    i = 0
+    for x in range(-max_x + 1, max_x):
+        while i < n and sorted_xs[i] < x:
+            i += 1
+        # x_sum(x) = Sum(|x-xj|) = 1_{0<=j<=i_x} Sum(x-xj) + 1_{i_x<j<n} Sum(xj-x)
+        # = 1_{0<=j<=i_x} Sum(x-1 - xj + 1) + 1_{i_x<j<n} Sum(xj - (x+1) + 1)
+        # = 1_{0<=j<=i_x} Sum(x-1 - xj) + 1_{i_x<j<n} Sum(xj - x) + 1_{i_x<j<n} - 1_{0<=j<=i_x}
+        # x_sum(x-1)
+        # = Sum(|x-1-xj|) = 1_{0<=j<=i_(x-1)} Sum(x-1-xj) + 1_{i_(x-1)<j<n} Sum(xj-x+1)
+        # ここで i_(x-1) <= i_x であることに注意して式変形をすると
+        # x_sum(x) - x_sum(x-1) = (x-1 - x_(i_x)) - (x_(i_x) - (x-1)) + 1_{i_x<j<n} - 1_{0<=j<=i_x}
+        # = n - i_x - i_x
+        x_sum[x] = x_sum[x - 1] + i - (n - i)
+    return x_sum
 
-print(count)
+x_sum = calc(x)
+y_sum = calc(y)
+
+# 尺取り法は、ある区間に対して、その区間を左端から右端まで動かすことで、その区間に対する条件を満たす区間を求めるアルゴリズムです。
+# そのため、以下でソートを行うことで、x_sum, y_sum に対して尺取り法を行うことができます。
+x_sum.sort()
+y_sum.sort()
+
+ans = 0
+y_index = 0
+
+for x_index in range(len(x_sum))[::-1]:
+    # x_index - 1 よりも x_index のほうが x_sum の値が小さいため もっと y_index を進められるか確かめる
+    while y_index < len(y_sum) and y_sum[y_index] + x_sum[x_index] <= d:
+        y_index += 1
+    ans += y_index
+
+print(ans)
+
+
