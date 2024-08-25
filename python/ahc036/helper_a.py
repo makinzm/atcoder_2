36f0c7a69f0d6d5f95016a10a662129de43e4351
makinzm <nozomi.maki.da@gmail.com>
2024-08-25 20:13:26 +0900

    fix: update helper file

--- python/ahc036/helper_a.py
+++ python/ahc036/helper_a.py
@@ -3,30 +3,28 @@ import sys
 
 command = ["rye", "run", "python3", "a.py"]
 
-input_data = """7 9 3 7 4
-0 1
-0 2
-0 3
-1 2
-2 3
-3 4
-4 5
-5 6
-6 0
-4 1 5
-100 0
-200 0
-200 100
-100 100
-0 200
-0 100
-0 0"""
+try:
+    with open("input.txt") as file:
+        input_data = file.read()
+except FileNotFoundError:
+    print("Error: input.txt file not found.", file=sys.stderr)
+    sys.exit(1)
+except OSError:
+    print("Error: Could not read input.txt file.", file=sys.stderr)
+    sys.exit(1)
 
 process = subprocess.Popen(command, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
 
 stdout, stderr = process.communicate(input_data)
 
-print(stdout)
+try:
+    with open("output.txt", "w") as file:
+        file.write(stdout)
+    print("Output has been written to output.txt")
+except OSError:
+    print("Error: Could not write to output.txt file.", file=sys.stderr)
+    sys.exit(1)
 
 if stderr:
+    print("Errors occurred:", file=sys.stderr)
     print(stderr, file=sys.stderr)
