f6bb23d4b25dff689bd53f5d22d1c7b20085e5e0
makinzm <nozomi.maki.da@gmail.com>
2024-09-15 10:08:58 +0900

    Thinking contribution

--- python/abc371/e.py
+++ python/abc371/e.py
@@ -1,18 +1,27 @@
-from atcoder.segtree import SegTree
+from collections import defaultdict
 
 n = int(input())
 a = list(map(int, input().split()))
 
-seg = SegTree(
-    op = lambda x, y: x | y,
-    e = set(),
-    v = [set([x]) for x in a]
-)
+positions = defaultdict(list)
+for i, v in enumerate(a):
+    positions[v].append(i)
 
-ans = 0
-for i in range(n):
-    for j in range(i+1, n+1):
-        ans += len(seg.prod(i, j))
+total = 0
+for value, position_list in positions.items():
+    previous_position = -1
+    for position in position_list:
+        # The times when the position is the first time x The times when the distance from the last one
+        # for example, 1 2 1 2 1 2 1 2 1 2
+        # value = 1, position_list = [0, 2, 4, 6, 8]
+        # value = 2, position_list = [1, 3, 5, 7, 9]
+        # value = 1, position = 0, previous_position = -1, added_total = 1 * 10 = 10
+        # [1], [1,2], [1,2,1], [1,2,1,2], [1,2,1,2,1], ..., [1,2,1,2,1,2,1,2,1,2]
+        # value = 1, position = 2, previous_position = 0, added_total = 2 * 8 = 16
+        # [-,2,1], [-,2,1,2], [-,2,1,2,1], [-,2,1,2,1,2], ..., [-,2,1,2,1,2,1,2,1,2]
+        # [-,-,1], [-,-,1,2], [-,-,1,2,1], [-,-,1,2,1,2], ..., [-,-,1,2,1,2,1,2,1,2]
+        total += (position - previous_position) * (n - position)
+        previous_position = position
 
-print(ans)
+print(total)
 
